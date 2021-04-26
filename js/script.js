81009230652ab9bba4ab57b4a6857d0440fc40fa
nanifour <nholloway013@gmail.com>
2021-04-25 23:03:17 -0400

    changed the simple color flipper to a rgb color flipper

--- js/script.js
+++ js/script.js
@@ -1,35 +1,32 @@
-//What kind of colors will be used: colors, rgb, and hex
-const colors = ["green", "red", "blue", "yellow", 
-                "rgba(133,122,200)", "rgba(0,255,0)", 
-                "#f15025", "#7f96d5", "#ef7bd7", "#ff8500",
-                "#7400b8", "#beb7a4", "#ff4800", "#29f7ff"];
-
-//https://developer.mozilla.org/en-US/docs/Web/API/Document
-
 //get button id
 const button = document.getElementById('btn');
 //get color
 const color = document.querySelector('.color');
 
+
 //everytime that the button is clicked, the background changes
 button.addEventListener('click', function(){
 
+    const randomRgba = getRandomRgb();
     //use background color property to change color
-
-    //get random num between 0(green) and 3(f15025) in array
-    const randomNumber = genRandomNum();
-    document.body.style.backgroundColor = colors[randomNumber];
-    color.textContent = colors[randomNumber];
-
+    color.textContent = randomRgba;
+    document.body.style.backgroundColor = randomRgba;
 
 });
 
-function genRandomNum(){
-    //multiply by length to get a number that is not just between 0 & 1 
-     // then round down to closest integer
-    return Math.floor(Math.random() * colors.length);
 
-}
+function getRandomRgb() {
+    //generates a number between 0 and 0xfffff (or 2^24, the highest number you can get from 24 bits)
+    var num = Math.round(0xffffff * Math.random());
+    //red value (left-most 8 bits)
+    var r = num >> 16;
+    //green value (middle-8 bits)
+    var g = num >> 8 & 255;
+    //blue value (last 8 bits)
+    var b = num & 255; 
+    //return rbg and random values
+    return 'rgb(' + r + ', ' + g + ', ' + b + ')';
+  }
 
 
 
